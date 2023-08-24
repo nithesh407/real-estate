015a3b0122f88a49630c7cd71a4b340d56649327
NITHESH1006 <nitheshravikumar13631@gmail.com>
2023-08-24 23:12:26 +0530

    american brokers

--- app.py
+++ app.py
@@ -5,10 +5,17 @@ from flask import Flask, render_template, request, send_file
 app = Flask(__name__)
 
 
-@app.route('/',)
-def home():
+@app.route('/')
+def login():
    
 
+    return render_template("login.html")
+@app.route('/register')
+def register():
+    return render_template("register.html")
+
+@app.route('/index')
+def home():
     return render_template("index.html")
 
 if __name__ == "__main__":
