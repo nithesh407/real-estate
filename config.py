43fff2e239950a01c883492d437a7fab963b9dc6
NITHESH1006 <nitheshravikumar13631@gmail.com>
2023-09-03 11:52:37 +0530

    changes in requirements.txt

---
+++ config.py
@@ -0,0 +1,5 @@
+class Config:
+    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:nithesh13631@localhost:3306/nithesh'
+    SQLALCHEMY_TRACK_MODIFICATIONS=False
+    SECRET_KEY = 'your_secret_key'
+    DEBUG=True
