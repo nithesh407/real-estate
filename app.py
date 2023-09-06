1dab544adc6d6d7029a23b9d5337347b7c134c23
NITHESH1006 <nitheshravikumar13631@gmail.com>
2023-09-06 23:29:46 +0530

    finished almost

--- app.py
+++ app.py
@@ -54,25 +54,141 @@ def login():
 def profile():
     return render_template('index.html')
 
-@app.route('/prithivi')
+@app.route('/prithivi',methods=['GET','POST'])
 def prithivi():
+    if request.method == 'POST':
+        firstname = request.form['firstname']
+        lastname = request.form['lastname']
+        email = request.form['email']
+        message = request.form['message']
+
+        # You can perform any validation or additional checks here
+
+        new_user = models.Clients(
+            firstname=firstname,
+            lastname=lastname,
+            email=email,
+            message=message
+        )
+        db.session.add(new_user)
+        db.session.commit()
+        flash('Registration successful. Please log in.', 'success')
     return render_template('prithivi.html')
 
-@app.route('/pranesh')
+@app.route('/pranesh',methods=['GET','POST'])
 def pranesh():
+    if request.method == 'POST':
+        firstname = request.form['firstname']
+        lastname = request.form['lastname']
+        email = request.form['email']
+        message = request.form['message']
+
+        # You can perform any validation or additional checks here
+
+        new_user = models.Clients(
+            firstname=firstname,
+            lastname=lastname,
+            email=email,
+            message=message
+        )
+        db.session.add(new_user)
+        db.session.commit()
+        flash('Registration successful. Please log in.', 'success')
     return render_template('pranesh.html')
 
-@app.route('/Nithesh')
+@app.route('/Nithesh',methods=['GET','POST'])
 def nithesh():
+    if request.method == 'POST':
+        firstname = request.form['firstname']
+        lastname = request.form['lastname']
+        email = request.form['email']
+        message = request.form['message']
+
+        # You can perform any validation or additional checks here
+
+        new_user = models.Clients(
+            firstname=firstname,
+            lastname=lastname,
+            email=email,
+            message=message
+        )
+        db.session.add(new_user)
+        db.session.commit()
+        flash('Registration successful. Please log in.', 'success')
     return render_template('nithesh.html')
 
-@app.route('/nix')
+@app.route('/nix',methods=['GET','POST'])
 def nix():
+    if request.method == 'POST':
+        firstname = request.form['firstname']
+        lastname = request.form['lastname']
+        email = request.form['email']
+        message = request.form['message']
+
+        # You can perform any validation or additional checks here
+
+        new_user = models.Clients(
+            firstname=firstname,
+            lastname=lastname,
+            email=email,
+            message=message
+        )
+        db.session.add(new_user)
+        db.session.commit()
+        flash('Registration successful. Please log in.', 'success')
     return render_template('nix.html')
 
 @app.route('/agents')
 def agents():
     return render_template('agents.html')
+
+@app.route('/property')
+def property():
+    return render_template('property-grid.html')
+
+@app.route('/propertysingle',methods=['GET','POST'])
+def propertysingle():
+    if request.method == 'POST':
+        
+        name = request.form['name']
+        email = request.form['email']
+        comment = request.form['comment']
+
+        # You can perform any validation or additional checks here
+
+        new_user = models.Agent(
+            
+            name=name,
+            email=email,
+            comment=comment
+        )
+        db.session.add(new_user)
+        db.session.commit()
+           
+    return render_template('property-single.html')
+
+@app.route('/contact',methods=['GET','POST'])
+def contact():
+    if request.method == 'POST':
+        
+        name = request.form['name']
+        email = request.form['email']
+       
+        comment = request.form['comment']
+
+        # You can perform any validation or additional checks here
+
+        new_user = models.Contact(
+            
+            name=name,
+            email=email,
+        
+            comment=comment
+        )
+        db.session.add(new_user)
+        db.session.commit()
+           
+    return render_template('contact.html')
 
 if __name__ == '__main__':
     app.run()
--- models.py
+++ models.py
@@ -5,3 +5,24 @@ class User(db.Model):
 
     username = db.Column(db.String(80), unique=True, nullable=False, primary_key=True)
     password = db.Column(db.String(300), nullable=False)
+
+class Clients(db.Model):
+    __tablename__='clients'
+    firstname = db.Column(db.String(80), nullable=False)
+    lastname = db.Column(db.String(80), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False,primary_key=True)
+    message = db.Column(db.Text)
+
+class Agent(db.Model):
+    __tablename__='agent'
+    
+    name = db.Column(db.String(80), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False,primary_key=True)
+    comment = db.Column(db.Text)   
+class Contact(db.Model):
+    __tablename__='contact'
+    
+    name = db.Column(db.String(80), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False,primary_key=True)
+    
+    comment = db.Column(db.Text)      
